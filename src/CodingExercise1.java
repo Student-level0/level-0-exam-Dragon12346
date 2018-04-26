174d1d28c4344d64de8b253504b4e41d2556f9d4
Dragon12346 <league@iMac-2.attlocal.net>
2018-04-25 18:24:22 -0700

    CodingExercise1&2

--- src/CodingExercise1.java
+++ src/CodingExercise1.java
@@ -5,15 +5,31 @@ import org.jointheleague.graphical.robot.Robot;
 /* Level 0 Exam: Coding  Exercise #1 */
 public class CodingExercise1 {
 
-    public static void main(String[] args) {
-   	 // 3. ask the user what color they would like the Robot to draw
+	public static void main(String[] args) {
+		Robot r = new Robot();
+		// 3. ask the user what color they would like the Robot to draw
+		r.hide();
+		String colormuch = JOptionPane.showInputDialog("What color do you choose?(black or blue)");
+		if (colormuch.equals("blue")) {
 
-   	 // 4. use an if/else statement to set the pen color that the user requested (minimum of 2 colors)
+			r.setPenColor(Color.blue);
+		}
+		r.penDown();
+		r.setPenWidth(10);
+		r.setSpeed(100);
 
-   	 // 2. set the pen width to 10
+		for (int i = 0; i < 4; i++) {
+			r.move(100);
+			r.turn(90);
+		}
 
-   	 // 1. make the Robot draw a shape
-   	 
-    }
+		// 4. use an if/else statement to set the pen color that the user requested
+		// (minimum of 2 colors)
+
+		// 2. set the pen width to 10
+
+		// 1. make the Robot draw a shape
+
+	}
 
 }
--- src/CodingExercise2.java
+++ src/CodingExercise2.java
@@ -1,17 +1,21 @@
 
-
 /* Level 0 Exam: Coding  Exercise #2 */
 import javax.swing.JOptionPane;
 
 /**
- * Write a program that asks the user for their age. 
- * Tell them which year they were born. 
- * If they are over 30, tell them they are too old to play this game.
+ * Write a program that asks the user for their age. Tell them which year they
+ * were born. If they are over 30, tell them they are too old to play this game.
  **/
 
 public class CodingExercise2 {
+	public static void main(String[] args) {
+		String g = JOptionPane.showInputDialog("How old are you?");
+		int q = Integer.parseInt(g);
+		int w = 2018 - q;
+		JOptionPane.showMessageDialog(null, "You were born in " + w);
+		if (q > 30) {
+			JOptionPane.showMessageDialog(null, "You too old SONNNN!");
+		}
 
+	}
 }
-
-
-
